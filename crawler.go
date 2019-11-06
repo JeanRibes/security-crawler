c5bcdcd2a4c0dbf72f9ce73efb0097e188c513fc
Jean Ribes <jean.christophe.ribes@sfr.fr>
2019-11-06 13:51:08 +0100

    déduplication des résultats, moins de requêtes inutiles, indexeur plus rapide que le code récursif !

--- crawler.go
+++ crawler.go
@@ -12,15 +12,8 @@ import (
 )
 
 var relative_links_regex = regexp.MustCompile(`href="(?P<link>\S+)"`)
-var explored_links []string // pour ne pas tourner en rond il faut savoir d'où on vient
-var explored_links_lock sync.RWMutex
-
-var all_insecure_links []string
-var all_secure_links []string
-var total_links_list_lock sync.RWMutex
 
 var run = true // permet d'arrêter l'itération des boucles & la récursion
-var wg sync.WaitGroup
 
 func main() {
 	unix_chan := make(chan os.Signal, 1)   // merci stackoverflow
@@ -38,14 +31,15 @@ func main() {
 	if len(os.Args) == 2 {
 		to_index = os.Args[1]
 	}
-	crawing_loop(to_index, to_index)
+	//all_insecure_links, all_secure_links := crawing_loop(to_index, to_index)
+	all_insecure_links, _ := crawing_loop(to_index, to_index)
 
 	fmt.Println("-------------------------")
 	fmt.Println("Fin de l'indexation")
-	/*for _, l := range all_insecure_links {
+	for _, l := range all_insecure_links {
 		fmt.Println(l)
 	}
-	for _, l := range all_secure_links {
+	/*for _, l := range all_secure_links {
 		fmt.Println(l)
 	}*/
 }
@@ -60,42 +54,58 @@ func stringInStrings(string string, strings []string) bool {
 	return false
 }
 
-func crawing_loop(page string, root string) {
+func crawing_loop(page string, root string) ([]string, []string) {
+	wg := &sync.WaitGroup{}
+	explored_linksP := &[]string{}
+	//explored_links_lock := &sync.RWMutex{}
+	total_links_list_lockP := &sync.RWMutex{}
+	all_insecure_linksP := &[]string{}
+	all_secure_linksP := &[]string{}
 	to_explore := &[]string{root}
 	for len(*to_explore) > 0 {
 		next_loop_explore := &[]string{}
-		explored_links = append(explored_links, *to_explore...) // en fait on pourait nettoyer dans chaque goroutine
-		for _, link := range *to_explore {                      // on lance toutes les requetes en même temps
+		*explored_linksP = append(*explored_linksP, *to_explore...) // en fait on pourait nettoyer dans chaque goroutine
+		for _, link := range *to_explore {                          // on lance toutes les requetes en même temps
 			wg.Add(1)
-			go poolcrawl(link, root, next_loop_explore)
+			go poolcrawl(link, root, next_loop_explore, explored_linksP, all_insecure_linksP, all_secure_linksP, total_links_list_lockP, wg)
 		}
 		wg.Wait()
-		fmt.Println("batch finie")
+		fmt.Println("batch finie, dédupication des URL")
+		*all_secure_linksP = deduplicateur(*all_secure_linksP)
+		*all_insecure_linksP = deduplicateur(*all_insecure_linksP)
 		fmt.Println(len(*next_loop_explore))
-		deduplicateur(*next_loop_explore)
+		fmt.Println("nouvelle batch")
+		*next_loop_explore = deduplicateur(*next_loop_explore)
 		fmt.Println(len(*next_loop_explore))
 		to_explore = next_loop_explore // on échange les variables
 	}
+	/*print("deddup finale")
+	deduplicateur(*all_secure_linksP)
+	deduplicateur(*all_insecure_linksP)*/
+
+	return *all_insecure_linksP, *all_secure_linksP
 }
 
 var to_explore_lock sync.RWMutex
 
-func poolcrawl(page string, root string, to_explore *[]string) {
+func poolcrawl(page string, root string, to_explore *[]string, explored_linksP *[]string, all_insecure_linksP *[]string, all_secure_linksP *[]string, total_links_list_lockP *sync.RWMutex, wg *sync.WaitGroup) {
 	defer wg.Done()
 	if strings.Contains(page, root) && run {
 		insecure_links_page, secure_links_page := crawl(page, root)
-		total_links_list_lock.Lock()
-		all_insecure_links = append(all_insecure_links, insecure_links_page...) // on enregistre les liens http
-		all_secure_links = append(all_secure_links, secure_links_page...)
-		total_links_list_lock.Unlock()
+		total_links_list_lockP.Lock()
+		*all_insecure_linksP = append(*all_insecure_linksP, insecure_links_page...) // on enregistre les liens http
+		*all_secure_linksP = append(*all_secure_linksP, secure_links_page...)
+		total_links_list_lockP.Unlock()
 		/*fmt.Println(len(explored_links))
 		fmt.Println(len(secure_links_page))
 		fmt.Println(len(*to_explore))*/
 
 		var local_to_add []string
 		for _, link := range secure_links_page {
-			if strings.Contains(link, root) {
-				local_to_add = append(local_to_add, link)
+			if strings.Contains(link, root) { // on reste sur le même site
+				if !stringInStrings(link, *explored_linksP) { // on va pas 2 fois sur la même page
+					local_to_add = append(local_to_add, link)
+				}
 			}
 		}
 		to_explore_lock.Lock()
@@ -109,7 +119,7 @@ func deduplicateur(multiples []string) []string {
 	for _, elem := range multiples {
 		unique := true
 		for _, uniq := range uniques { // si elem n'est pas dans uniques
-			if uniq == elem { //si on le trouve
+			if strings.Compare(uniq, elem) == 0 { //si on le trouve
 				unique = false
 			}
 		}
@@ -118,23 +128,6 @@ func deduplicateur(multiples []string) []string {
 		}
 	}
 	return uniques
-}
-func recurse(link string, root string, total_insecure_links []string, total_secure_links []string, n int) (insecure_links []string, secure_links []string) {
-
-	insecure_links_page, secure_links_page := crawl(link, root)
-	explored_links = append(explored_links, link)
-	total_insecure_links = append(total_insecure_links, insecure_links_page...) // on enregistre les liens http
-
-	for _, link := range secure_links_page { // on parcourt tous les liens HTTPS pour trouver les liens http
-		total_secure_links = append(total_secure_links, link)
-		if strings.Contains(link, root) && run {
-			// si on reste dans le même site
-			if !stringInStrings(link, explored_links) && n > 0 { // si on a pas déjà visité le lien et qu'on a le droit de boucler
-				recurse(link, root, total_insecure_links, total_secure_links, n-1) // on continue
-			}
-		}
-	}
-	return total_insecure_links, total_secure_links
 }
 
 func extract_hrefs(page []byte, current_page string, root string) (http_links []string, https_links []string) {
